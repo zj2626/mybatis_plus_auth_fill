535d3e2774ecf9cfab1de65fb6550eaf362127ec
zj2626 <ay180629@outlook.com>
2021-03-12 09:21:11 +0800

    简化

--- src/main/java/com/mybatisplus/parent/base/entity/BaseEntity.java
+++ src/main/java/com/mybatisplus/parent/base/entity/BaseEntity.java
@@ -1,14 +1,57 @@
 package com.mybatisplus.parent.base.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
+import java.io.Serializable;
+import java.util.Date;
+
 @Data
-public class BaseEntity extends AbstractBaseEntity {
+public abstract class BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @TableId(type = IdType.AUTO) // 主键生成策略
     private Long id;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 创建人
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private String createUser;
+
+    /**
+     * 创建人ID
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUserId;
+
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 更新人
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updateUser;
+
+    /**
+     * 更新人ID
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUserId;
+
+    // 逻辑删除 删除状态 0已删除 1未删除
+    @TableLogic(value = "true", delval = "false")
+    @TableField(fill = FieldFill.INSERT)
+    private Boolean deleteStatus;
 }
