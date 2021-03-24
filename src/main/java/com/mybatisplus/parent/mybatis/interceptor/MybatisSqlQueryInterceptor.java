7589d9797f862d447b6c8b536428c6a8f198bab0
zj2626 <ay180629@outlook.com>
2021-03-24 23:12:51 +0800

    去除异常捕获

--- src/main/java/com/mybatisplus/parent/mybatis/interceptor/MybatisSqlQueryInterceptor.java
+++ src/main/java/com/mybatisplus/parent/mybatis/interceptor/MybatisSqlQueryInterceptor.java
@@ -43,73 +43,68 @@ public class MybatisSqlQueryInterceptor implements Interceptor {
      */
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        try {
-            Object[] args = invocation.getArgs();
-            MappedStatement ms = (MappedStatement) args[0];
-            Object parameterObject = args[1];
+        Object[] args = invocation.getArgs();
+        MappedStatement ms = (MappedStatement) args[0];
+        Object parameterObject = args[1];
 
-            // id为执行的mapper方法的全路径名，如com.mapper.UserMapper
-            String id = ms.getId();
+        // id为执行的mapper方法的全路径名，如com.mapper.UserMapper
+        String id = ms.getId();
 
-            // sql语句类型 select、delete、insert、update
-            String sqlCommandType = ms.getSqlCommandType().toString();
+        // sql语句类型 select、delete、insert、update
+        String sqlCommandType = ms.getSqlCommandType().toString();
 
-            BoundSql boundSql = ms.getBoundSql(parameterObject);
-            String origSql = boundSql.getSql();
+        BoundSql boundSql = ms.getBoundSql(parameterObject);
+        String origSql = boundSql.getSql();
 
-            origSql = origSql.replace("where", WHERE);
-            if (!origSql.contains(WHERE)) {
-                return invocation.proceed();
-            }
-            String[] splits = origSql.split(WHERE);
-            if (splits.length < 2) {
-                return invocation.proceed();
-            }
-            if (origSql.contains(StringKeyCode.COMPANY_ID_PARAM) && origSql.lastIndexOf(StringKeyCode.COMPANY_ID_PARAM) > origSql.indexOf(WHERE)) {
-                return invocation.proceed();
-            }
-
-            try {
-                final Class<?> entityType = ms.getResultMaps().get(0).getType();
-                if (null != entityType) {
-                    if (null == checkFieldWithTableQueryField(entityType)) {
-                        return invocation.proceed();
-                    }
-                }
-            } catch (Exception e) {
-                return invocation.proceed();
-            }
-
-            // 组装新的 sql
-            StringBuilder newSql = new StringBuilder();
-            for (int i = 0; i < splits.length; i++) {
-                if (i == splits.length - 1) {
-                    newSql.append(WHERE).append(" ").append(StringKeyCode.COMPANY_ID_PARAM).append(" = ").append(CommonAdminUtils.getCompanyIdFromUserInfoBo()).append(" AND ");
-                }
-                newSql.append(splits[i]);
-            }
-
-            // 重新new一个查询语句对象
-            BoundSql newBoundSql = new BoundSql(ms.getConfiguration(), newSql.toString(),
-                    boundSql.getParameterMappings(), boundSql.getParameterObject());
-
-            // 把新的查询放到statement里
-            MappedStatement newMs = newMappedStatement(ms, new BoundSqlSqlSource(newBoundSql));
-            for (ParameterMapping mapping : boundSql.getParameterMappings()) {
-                String prop = mapping.getProperty();
-                if (boundSql.hasAdditionalParameter(prop)) {
-                    newBoundSql.setAdditionalParameter(prop, boundSql.getAdditionalParameter(prop));
-                }
-            }
-
-            Object[] queryArgs = invocation.getArgs();
-            queryArgs[0] = newMs;
-
-            return invocation.proceed();
-        } catch (Exception e) {
-            logger.error("sql拦截器发生异常: {}", e.getMessage(), e);
+        origSql = origSql.replace("where", WHERE);
+        if (!origSql.contains(WHERE)) {
             return invocation.proceed();
         }
+        String[] splits = origSql.split(WHERE);
+        if (splits.length < 2) {
+            return invocation.proceed();
+        }
+        if (origSql.contains(StringKeyCode.COMPANY_ID_PARAM) && origSql.lastIndexOf(StringKeyCode.COMPANY_ID_PARAM) > origSql.indexOf(WHERE)) {
+            return invocation.proceed();
+        }
+
+        try {
+            final Class<?> entityType = ms.getResultMaps().get(0).getType();
+            if (null != entityType) {
+                if (null == checkFieldWithTableQueryField(entityType)) {
+                    return invocation.proceed();
+                }
+            }
+        } catch (Exception e) {
+            return invocation.proceed();
+        }
+
+        // 组装新的 sql
+        StringBuilder newSql = new StringBuilder();
+        for (int i = 0; i < splits.length; i++) {
+            if (i == splits.length - 1) {
+                newSql.append(WHERE).append(" ").append(StringKeyCode.COMPANY_ID_PARAM).append(" = ").append(CommonAdminUtils.getCompanyIdFromUserInfoBo()).append(" AND ");
+            }
+            newSql.append(splits[i]);
+        }
+
+        // 重新new一个查询语句对象
+        BoundSql newBoundSql = new BoundSql(ms.getConfiguration(), newSql.toString(),
+                boundSql.getParameterMappings(), boundSql.getParameterObject());
+
+        // 把新的查询放到statement里
+        MappedStatement newMs = newMappedStatement(ms, new BoundSqlSqlSource(newBoundSql));
+        for (ParameterMapping mapping : boundSql.getParameterMappings()) {
+            String prop = mapping.getProperty();
+            if (boundSql.hasAdditionalParameter(prop)) {
+                newBoundSql.setAdditionalParameter(prop, boundSql.getAdditionalParameter(prop));
+            }
+        }
+
+        Object[] queryArgs = invocation.getArgs();
+        queryArgs[0] = newMs;
+
+        return invocation.proceed();
     }
 
     public static Field checkFieldWithTableQueryField(Class<?> entityType) throws NoSuchFieldException {
